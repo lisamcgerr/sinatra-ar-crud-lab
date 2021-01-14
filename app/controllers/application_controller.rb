11d928b063729ec5e07d141132e8ab1a9cb73158
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-14 10:16:07 -0600

    commit tree c815e994c346bab69ac3bb2711d63406f923b7c5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,37 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    @articles = Article.all 
+    erb :new
   end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to '/articles/#{@article.id}'
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    @article.save
+    redirect to '/articles#/{@article.id}'
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect to '/articles'
+  end
+
 end
